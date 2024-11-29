fe9b7c73abe9317fe5259ff961772cef53b738d9
ShernOC <sherlynea1994@hotmail.com>
2024-11-29 12:19:48 +0300

    fibo sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,25 @@
-#!/usr/bin/env python3
-
+# !/usr/bin/env python3
+# #fibonacci sequence = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...]
+# #prints out fibonacci sequence 
+# #formula 
+# #Xn = Xn-1 + Xn-2
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+            print([]) 
+    elif length ==1: 
+        print ([0])
+    elif length == 2: 
+        print ([0,1]) 
+    else:
+        fibo_list= [0, 1]
+        for _ in range(2,length):
+            fibo_list.append(fibo_list[-1]+ fibo_list[-2])
+        print(fibo_list)
+      
+print_fibonacci(9)
+
+
+
+
+
+
